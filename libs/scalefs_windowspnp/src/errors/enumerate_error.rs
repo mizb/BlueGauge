91c8020fa213329896a471e7f2c36d78b2544153
iKineticate <115683118+iKineticate@users.noreply.github.com>
2025-07-24 23:59:47 +0800

    Update enumerate_error.rs

--- libs/scalefs_windowspnp/src/errors/enumerate_error.rs
+++ libs/scalefs_windowspnp/src/errors/enumerate_error.rs
@@ -1,6 +1,7 @@
 // Copyright (c) ScaleFS LLC; used with permission
 // Licensed under the MIT License
 
+#[derive(Debug)]
 pub enum EnumerateError {
     StringDecodingError(/*error: */std::string::FromUtf16Error),
     StringTerminationDecodingError,
