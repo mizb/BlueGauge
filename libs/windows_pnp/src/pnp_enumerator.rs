afa026f047cabf94e8433521fb2b54a09abe2263
iKineticate <115683118+iKineticate@users.noreply.github.com>
2025-08-11 20:38:38 +0800

    fix: 上一个修复错了，哈哈哈

--- libs/windows_pnp/src/pnp_enumerator.rs
+++ libs/windows_pnp/src/pnp_enumerator.rs
@@ -130,6 +130,7 @@ impl PnpEnumerator {
                 flags |= DIGCF_DEVICEINTERFACE;
             },
             EnumerateSpecifier::DeviceSetupClassGuid(setup_class_guid) => {
+                std::hint::black_box(&setup_class_guid);  // 请不要动这里，这个可以修复Release模式下的BUG
                 pnp_enumerator = None;
                 class_guid = Some(&setup_class_guid);
                 device_interface_class_guid = None;
